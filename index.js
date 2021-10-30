ee92d121751d12331bf7b93f65afd7295594f6fa
alidaexpert <addafsar@gmail.com>
2021-10-30 23:52:58 +0600

    Added

--- index.js
+++ index.js
@@ -12,8 +12,31 @@ app.use(cors())
 app.get("/",(req,res)=>{
     res.send("Successfully Run The Node And Express")
 })
+app.get("/check",(req,res)=>{
+    res.send("Check  this for remote url change")
+})
 
+// Mongo connect 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwcfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("Tour-Express");
+      const offers = database.collection("offers");
+      // create a document to insert
+    //   const doc = {
+    //     title: "Record of a Shriveled Datum",
+    //     content: "No bytes, no problem. Just insert a document, in MongoDB",
+    //   }
+    //   const result = await haiku.insertOne(doc);
+    //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.listen(port,()=>{
     console.log("Running the Localhost",`http://localhost:${port}`)
